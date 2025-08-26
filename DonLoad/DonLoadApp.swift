3ba9b4cbea32ba94e017d3d6623975b47ef36f88
kq62 <kq62@cornell.edu>
2025-08-26 13:32:26 -0400

    Initial Commit

---
+++ DonLoad/DonLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonLoadApp.swift
+//  DonLoad
+//
+//  Created by Kuangming Qin on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DonLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
